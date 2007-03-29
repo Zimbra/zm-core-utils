882cebe8cd770016202b89a77ad4a81c89de1bfd
Unknown Perforce User <schemers>
2007-03-29 13:28:55 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=15709 Integrate change 48539 from main to FRANK
    	----------
    	Change 48539 by schemers@macpro-home on 2007/03/29 11:26:42
    
    		http://bugzilla.zimbra.com/show_bug.cgi?id=15709
    		bug: 15709
    		- createFolder --url ... was not passing the URL to the createFolder call (second try, the --url option is also used by two admin commands)
    
    	Affected files ...
    
    	... //depot/main/ZimbraServer/src/java/com/zimbra/cs/zclient/ZMailboxUtil.java#88 edit
    
    Copied from Perforce
     Change: 48540

--- src/perl/soap/search.pl
+++ src/perl/soap/search.pl
@@ -35,7 +35,6 @@ use ZimbraSoapTest;
 
 # specific to this app
 my ($searchString, $offset, $prevId, $prevSortVal, $endSortVal, $limit, $fetch, $sortBy, $types, $convId, $tz, $locale, $field);
-my ($calExpandInstStart, $calExpandInstEnd);
 $offset = 0;
 $limit = 5;
 $fetch = 0;
@@ -62,8 +61,6 @@ GetOptions("u|user=s" => \$user,
            "tz=s" => \$tz,
            "locale=s" => \$locale,
            "field=s" => \$field,
-           "calExpandInstStart=s" => \$calExpandInstStart,
-           "calExpandInstEnd=s" => \$calExpandInstEnd,
           );
 
 
@@ -71,7 +68,7 @@ GetOptions("u|user=s" => \$user,
 if (!defined($user) || !defined($searchString) || defined($help)) {
     my $usage = <<END_OF_USAGE;
     
-USAGE: $0 -u USER -q QUERYSTR [-s SORT] [-t TYPES] [-o OFFSET] [-l LIMIT] [-f FETCH] [-pi PREV-ITEM-ID -ps PREV-SORT-VALUE] [-es END-SORT-VALUE] [-c CONVID] [-tz TZID] [-l LOCALE] [-calExpandInstStart STARTTIME -calExpandInstEnd ENDTIME]
+USAGE: $0 -u USER -q QUERYSTR [-s SORT] [-t TYPES] [-o OFFSET] [-l LIMIT] [-f FETCH] [-pi PREV-ITEM-ID -ps PREV-SORT-VALUE] [-es END-SORT-VALUE] [-c CONVID] [-tz TZID] [-l LOCALE]
     SORT = dateDesc|dateAsc|subjDesc|subjAsc|nameDesc|nameAsc|score
     TYPES = message|conversation|contact|appointment
 END_OF_USAGE
@@ -90,12 +87,6 @@ my %args =  ( 'types' => $types,
               'limit' => $limit,
               'fetch' => $fetch
             );
-
-if (defined($calExpandInstStart)) {
-  $args{'calExpandInstStart'} = $calExpandInstStart;
-  $args{'calExpandInstEnd'} = $calExpandInstEnd;
-}
-  
 
 if (defined($convId)) {
   $searchName = "SearchConvRequest";
