6867e02a2b56c4c7dab12b27cebf59af44245d93
Unknown Perforce User <brian>
2007-07-12 18:09:37 -0500

    Integrated the following from //depot/main/...: Change 59163 by brian@brian-main on 2007/07/12 15:08:45
    bug: 17914
    added a new status script zmmtastatus to check postfix status. A separate script was added because the master process runs as root and the postfix/spool/pid/master.pid file is only readable by root. 
    added zmmtastatus to sudoers list 
    modified postfix status command to use new script instead of calling postqueue to check the postfix status.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=17914
    
    Copied from Perforce
     Change: 59177

--- src/perl/soap/nop.pl
+++ src/perl/soap/nop.pl
@@ -34,61 +34,33 @@ use Soap;
 use ZimbraSoapTest;
 
 #standard options
-my ($sessionId, $authToken, $notSeq, $user, $pw, $host, $help, $verbose);  #standard
-my ($wait);
-
+my ($user, $pw, $host, $help, $verbose);  #standard
 GetOptions("u|user=s" => \$user,
            "pw=s" => \$pw,
            "h|host=s" => \$host,
            "help|?" => \$help,
-           "w" => \$wait,
-           "v" => \$verbose,
-           "sessionId=s" => \$sessionId,
-           "at=s" => \$authToken,
-           "ns=s" => \$notSeq,
+           "v" => \$verbose
           );
+
+
 
 if (!defined($user)) {
   my $usage = <<END_OF_USAGE;
     
-USAGE: $0 -u USER [-w] [-v] [-ns notseq] [-at authToken]
+USAGE: $0 -u USER 
 END_OF_USAGE
   die $usage;
 }
 
-my %soapargs;
-$soapargs{ 'NOTIFY'} = 1;
-
-if (defined($sessionId)) {
-  $soapargs{'SESSIONID'} = $sessionId;
-}
-if (defined($notSeq)) {
-  $soapargs{'NOTSEQ'} = $notSeq;
-}
-
-my $z = ZimbraSoapTest->new($user, $host, $pw, \%soapargs);
+my $z = ZimbraSoapTest->new($user, $host, $pw, { NOTIFY => '1' } );
 $z->verbose(3);
-
-if (defined($sessionId) && defined($authToken)) {
-  $z->setAuthContext($authToken, $sessionId, \%soapargs);
-} else {
-  print "AUTH REQUEST:\n--------------------";
-  $z->doStdAuth();
-}
+$z->doStdAuth(); 
 
 my $d = new XmlDoc;
+$d->add('NoOpRequest', $Soap::ZIMBRA_MAIL_NS);
 
-my %args = ( );
-
-if (defined($wait)) {
-  $args{'wait'} = '1';
-}
-
-$d->add('NoOpRequest', $Soap::ZIMBRA_MAIL_NS, \%args);
-
-print "\n\nNOP:\n--------------------";
 my $response = $z->invokeMail($d->root());
 
-#print "REQUEST:\n-------------\n".$z->to_string_simple($d);
-#print "RESPONSE:\n--------------\n".$z->to_string_simple($response);
+print "REQUEST:\n-------------\n".$z->to_string_simple($d);
+print "RESPONSE:\n--------------\n".$z->to_string_simple($response);
 
