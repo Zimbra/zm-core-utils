87e26ea9f4bff151f8eb58008a60df1901f168dd
Unknown Perforce User <pfnguyen>
2008-10-02 15:43:20 -0500

    bug:31697
    zmcalchk: integrate notification to franklin
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31697
    
    Copied from Perforce
     Change: 117468

--- src/perl/threadDumpAnalyzer.pl
+++ src/perl/threadDumpAnalyzer.pl
@@ -23,7 +23,7 @@ my %locks;                      # by LockID - hash w/ 'owner','type'
 my %threads;           # by threadId - hash w/ 'state' 'waitingOnLock'
 
 my $filename = $ARGV[0];
-my ($dumpLocks, $dumpThreads, $searchThreadStack, $searchThreadId, $stackFrames, $sort, $filterByState, $allLocks, $negSearchThreadStack);
+my ($dumpLocks, $dumpThreads, $searchThreadStack, $searchThreadId, $stackFrames, $sort, $filterByState, $allLocks);
 my ($waiting);
 
 $stackFrames = 10;
@@ -37,7 +37,6 @@ GetOptions(
            "id=s" => \$searchThreadId,
            "state=s" => \$filterByState,
            "stack=s" => \$searchThreadStack,
-           "ns=s" => \$negSearchThreadStack,
            "waiting=s" => \$waiting,
            "all" => \$allLocks,
           );
@@ -263,8 +262,6 @@ sub dumpThreads() {
   foreach my $threadId ( sort { mySort($a, $b) } keys %threads ) {
     if (!defined $threadId) {
       # continue
-    } elsif (defined $negSearchThreadStack && ($threads{$threadId}{stack} =~ /$negSearchThreadStack/)) {
-      #continue
     } elsif (defined $searchThreadStack && !($threads{$threadId}{stack} =~ /$searchThreadStack/)) {
       # continue
     } elsif (defined $searchThreadId && !($threadId =~ /$searchThreadId/)) {
