a14047228e6fea399114f9a76aba711b6f03ff2a
Unknown Perforce User <brian>
2009-02-04 21:03:58 -0600

    Integrated the following from //depot/FRANKLIN/...: Change 144247 by brian@brianMP-franklin on 2009/02/04 19:03:34
    bug: 35026
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=35026
    
    Copied from Perforce
     Change: 144249

--- src/perl/soap/imModifyChat.pl
+++ src/perl/soap/imModifyChat.pl
@@ -16,53 +16,51 @@
 # ***** END LICENSE BLOCK *****
 # 
 
-#
-# Simple SOAP test-harness for the AddMsg API
-#
-
+use Date::Parse;
+use Time::HiRes qw ( time );
 use strict;
 
 use lib '.';
 
 use LWP::UserAgent;
 use Getopt::Long;
+use ZimbraSoapTest;
 use XmlElement;
 use XmlDoc;
 use Soap;
-use ZimbraSoapTest;
 
 #standard options
-my ($admin, $user, $pw, $host, $help, $adminHost); #standard
-my ($token);
+my ($user, $pw, $host, $help);  #standard
+my ($thread, $op);
+
 GetOptions("u|user=s" => \$user,
-           "admin" => \$admin,
-           "ah|adminHost=s" => \$adminHost,
            "pw=s" => \$pw,
            "h|host=s" => \$host,
            "help|?" => \$help,
-           "t=s" => \$token,
+           "thread=s" => \$thread,
+           "op=s" => \$op,
           );
 
-if (!defined($user) || defined($help)) {
-  my $usage = <<END_OF_USAGE;
-
-USAGE: $0 -u USER -t TOKEN
-END_OF_USAGE
-  die $usage;
+if (!defined($user) || (!defined($thread)) || (!defined($op))) {
+    print "USAGE: $0 -u USER -t thread -o op \n";
+    exit 1;
 }
 
-my $z = ZimbraSoapTest->new($user, $host, $pw, undef, $adminHost);
+my $z = ZimbraSoapTest->new($user, $host, $pw);
 $z->doStdAuth();
 
-my $SOAP = $Soap::Soap12;
-
 my $d = new XmlDoc;
-if (defined($token) && ($token ne "")) {
-    $d->start('SyncRequest', $Soap::ZIMBRA_MAIL_NS, { "token" => $token});
-} else {
-    $d->start('SyncRequest', $Soap::ZIMBRA_MAIL_NS);
-}
-$d->end(); # 'SyncRequest';'
+$d->start('IMModifyChatRequest', $Soap::ZIMBRA_IM_NS, { 'thread' => $thread, 'op'=>$op }); {
+  if ($op eq "configure") {
+    $d->add("var", undef, { 'name'=>"persistent" }, "false");
+    $d->add("var", undef, { 'name'=>"publicroom" }, "1");
+    $d->add("var", undef, { 'name'=>"moderated" }, "false");
+    $d->add("var", undef, { 'name'=>"semianonymous" }, "true");
+    $d->add("var", undef, { 'name'=>"noanonymous" }, "false");
+    $d->add("var", undef, { 'name'=>"password" }, "test123");
+    $d->add("var", undef, { 'name'=>"passwordprotect" }, "1");
+  }
+} $d->end();
 
 my $response = $z->invokeMail($d->root());
 
