a09d2b74991f5b2d31d152d2f3b82ae3b1105353
Unknown Perforce User <jhahm>
2009-02-11 14:30:33 -0600

    bug: 35259, 32675
    Integrating from FRANKLIN to main
    
    Change 145311 by jhahm@jhahm-mac3 on 2009/02/11 12:10:56
    
            bug: 35259, 32675
    
            Ability for a calendar resource partially accept a recurring 
            appointment when number of conflicts is within configured limit.
    
            Two preferences control the limits:
    
            * zimbraCalResMaxNumConflictsAllowed
            * zimbraCalResMaxPercentConflictsAllowed
    
            The first one sets the maximum *number* of conflicts allowed.  The 
            second one sets the maximum *percent* of conflicts allowed.  The 
            smaller of the max num and the number determined by the percent 
            parameter is used to determine whether the reject the meeting 
            request or not.
    
            Only conflicts in the future are counted.  For percent-based 
            calculation, the math is:
    
            # conflicts allowed = (total instances in the future) * (pct 
            configured) / 100
    
            If there are more future conflicts than this, we're over the limit.
    
            Both parameters default to 0, meaning even a single conflict results 
            in rejecting the whole recurring appointment.  This is consistent 
            with the old behavior.
    
            When the number of conflicts is small enough, the resource will do a 
            partial accept.  It updates its own status with ACCEPTED for the 
            recurrence series and DECLINED for each conflicting instance.  It 
            then sends one Accept reply to organizer, with a single iCalendar 
            REPLY part with ACCEPTED participation status.  The email body 
            explains the meeting was accepted but some dates were excluded, with 
            list of those dates.
    
            The resource also sends a second reply email to indicate declines.  
            The subject shows "Decline: ...", so the organizer gets extra 
            feedback that this appointment wasn't booked completely.  The replay 
            contains iCalendar REPLY object containing multiple VEVENT parts, 
            one for each declined instance.  The email body again shows the 
            declined dates, and also lists the conflicting meetings that are 
            already on resource's calendar.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=35259
    http://bugzilla.zimbra.com/show_bug.cgi?id=32675
    
    Copied from Perforce
     Change: 145318

--- src/perl/soap/gaal.pl
+++ src/perl/soap/gaal.pl
@@ -33,38 +33,32 @@ use ZimbraSoapTest;
 
 #standard options
 my ($admin, $user, $pw, $host, $help, $adminHost); #standard
-my ($token);
 GetOptions("u|user=s" => \$user,
            "admin" => \$admin,
            "ah|adminHost=s" => \$adminHost,
            "pw=s" => \$pw,
            "h|host=s" => \$host,
-           "help|?" => \$help,
-           "t=s" => \$token,
+           "help|?" => \$help
           );
 
 if (!defined($user) || defined($help)) {
   my $usage = <<END_OF_USAGE;
 
-USAGE: $0 -u USER -t TOKEN
+USAGE: $0 -u USER
 END_OF_USAGE
   die $usage;
 }
 
 my $z = ZimbraSoapTest->new($user, $host, $pw, undef, $adminHost);
-$z->doStdAuth();
+$z->doAdminAuth();
 
 my $SOAP = $Soap::Soap12;
 
 my $d = new XmlDoc;
-if (defined($token) && ($token ne "")) {
-    $d->start('SyncRequest', $Soap::ZIMBRA_MAIL_NS, { "token" => $token});
-} else {
-    $d->start('SyncRequest', $Soap::ZIMBRA_MAIL_NS);
-}
-$d->end(); # 'SyncRequest';'
+$d->start('GetAllAccountLoggersRequest', $Soap::ZIMBRA_ADMIN_NS);
+$d->end();
 
-my $response = $z->invokeMail($d->root());
+my $response = $z->invokeAdmin($d->root());
 
 print "REQUEST:\n-------------\n".$z->to_string_simple($d);
 print "RESPONSE:\n--------------\n".$z->to_string_simple($response);
