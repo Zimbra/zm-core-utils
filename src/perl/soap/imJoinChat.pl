ad6f66963173b070f0aa936d6f106a98db3808a5
Unknown Perforce User <pshao>
2009-02-12 23:13:18 -0600

    bug: 34899
    integrate change 145573 from main to FRANKLIN 
    ------------------------------------------------------
    Change 145573 by pshao@pshao-mac on 2009/02/12 21:08:23
    
            bug: 34899
    
            added account/cos duration attrs zimbraCalendarCalDavSyncStart, zimbraCalendarCalDavSyncEnd, zimbraCalendarCalDavSharedFolderCacheDuration.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=34899
    
    Copied from Perforce
     Change: 145575

--- src/perl/soap/imJoinChat.pl
+++ src/perl/soap/imJoinChat.pl
@@ -3,7 +3,7 @@
 # ***** BEGIN LICENSE BLOCK *****
 # 
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2005, 2007 Zimbra, Inc.
+# Copyright (C) 2007 Zimbra, Inc.
 # 
 # The contents of this file are subject to the Yahoo! Public License
 # Version 1.0 ("License"); you may not use this file except in
@@ -16,46 +16,48 @@
 # ***** END LICENSE BLOCK *****
 # 
 
-#
-# Simple SOAP test-harness for the AddMsg API
-#
-
-use Date::Parse;
-use Time::HiRes qw ( time );
 use strict;
-
 use lib '.';
 
 use LWP::UserAgent;
 use Getopt::Long;
-use ZimbraSoapTest;
-use XmlElement;
 use XmlDoc;
 use Soap;
+use ZimbraSoapTest;
+
+# specific to this app
+my ($threadId, $addr);
 
 #standard options
-my ($user, $pw, $host, $help);  #standard
-
-my $svc;
-
+my ($user, $pw, $host, $help); #standard
 GetOptions("u|user=s" => \$user,
            "pw=s" => \$pw,
            "h|host=s" => \$host,
            "help|?" => \$help,
-           "svc=s" => \$svc,
+           # add specific params below:
+           "t=s", \$threadId,
+           "a=s", \$addr
           );
 
-if (!defined($user)) {
-    print "USAGE: $0 -u USER -s CONFERENCE_SERVICE\n";
-    exit 1;
+
+
+if (!defined($user) || !defined($threadId) || defined($help)) {
+    my $usage = <<END_OF_USAGE;
+    
+USAGE: $0 -u USER -t threadId -a addr
+END_OF_USAGE
+    die $usage;
 }
 
 my $z = ZimbraSoapTest->new($user, $host, $pw);
 $z->doStdAuth();
 
 my $d = new XmlDoc;
-$d->start('IMListConferenceRoomsRequest', $Soap::ZIMBRA_IM_NS, { "svc" => $svc });
-$d->end(); #request
+my $searchName = "SearchRequest";
+
+$d->start("IMJoinChatRequest", $Soap::ZIMBRA_IM_NS, { 'addr' => $addr, 'thread' => $threadId });
+
+ $d->end(); 
 
 my $response = $z->invokeMail($d->root());
 
