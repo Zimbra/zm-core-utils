b9e3dffa74b3a28b7c4c719f851601e8f8007ae4
Unknown Perforce User <pburgu>
2012-12-06 11:39:13 -0600

    Integrating from main to HELIX. Change 425490 by pburgu@pburgu-main on 2012/12/06 09:35:12
            Integrating from IRONMAIDEN to main.
            Change 425489 by pburgu@pburgu-main on 2012/12/06 09:31:10
    
                    bug:78958
                    Construct index search filter correctly by embedding 
            multiple search 
                    terms within paranthesis.
                    i.e> Change the filter from  "email=test OR email2=test OR 
                    email3=test AND department=dept" to "((email=test OR 
            email2=test OR 
                    email3=test) AND department=dept)"
    
                    https://reviewboard.eng.vmware.com/r/407970/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=78958
    
    Copied from Perforce
     Change: 425491

--- src/perl/ldap/entrysize.pl
+++ src/perl/ldap/entrysize.pl
@@ -298,7 +298,7 @@ sub finishEntry() {
         finishAttr();
     }
 
-    if (@entryAttrMap) { # sanity checking for there could be extra empty lines
+    if (defined(@entryAttrMap)) { # sanity checking for there could be extra empty lines
         $dn = $entryAttrMap[0];
         
         if ($entryType ne "") {
