5686e51959ec9c1d6648fd1f93996faf6dd53a48
Unknown Perforce User <jflanigan>
2013-08-12 13:21:36 -0500

    Integrate change 479983 from main  bug: 83551
     Add LDAP attributes to disable HTTP compression. Both of these attributes default to TRUE since changing them to false will significantly increase network usage and impact web client performance.
    
     zimbraHttpCompressionEnabled - Used to enable/disable Jetty GzipFilter. Turning this off will prevent compression of most dynamic responses.
    
     zimbraWebGzipEnabled - Used to enable/disable compression of static web client resources such as JS, CSS, Zimlet resources, and skins.
    
     In general most aspects of the BREACH attack can be mitigated by setting zimbraHttpCompressionEnabled to FALSE while leaving zimbraWebGzipEnabled set to TRUE; however to be completely sure both can be set to FALSE.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83551
    
    Copied from Perforce
     Change: 479993

--- src/bin/ldapHelper.pl
+++ src/bin/ldapHelper.pl
@@ -1,8 +1,8 @@
-#!/bin/bash
-# 
+#!/usr/bin/perl
+#
 # ***** BEGIN LICENSE BLOCK *****
 # Zimbra Collaboration Suite Server
-# Copyright (C) 2012 VMware, Inc.
+# Copyright (C) 2010, 2011 Zimbra Software, LLC.
 # 
 # The contents of this file are subject to the Zimbra Public License
 # Version 1.3 ("License"); you may not use this file except in
@@ -12,6 +12,20 @@
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 # ***** END LICENSE BLOCK *****
-# 
+#
 
-exec `dirname $0`/zmjava com.zimbra.cs.store.file.BlobDeduperUtil "$@"
+use strict;
+no strict "refs";
+
+use Zimbra::Util::LDAP;
+
+sub logMsg{
+	print join (' ',@_),"\n";
+}
+
+our %config = (
+	ldap_is_master	=>	$ARGV[0],
+	ldap_root_password	=>	$ARGV[1],
+	);
+
+Zimbra::Util::LDAP->doLdap($ARGV[2],$ARGV[3]);
