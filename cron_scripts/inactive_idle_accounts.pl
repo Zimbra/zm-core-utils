3c66896d58840f9349360f76bb44b167fb50708e
uttamtakalkar <uttam.takalkar@gmail.com>
2018-08-02 10:07:04 +0530

    NICOPS-46 - Account Deactivation Script (#39)
    * NICOPS-46 - Account Deactivation Script
    
    * NICOPS-46 - ldap details not required so removing
    
    * added option to pass new account status
    
    * changes after code review
    
    * changes after code review
    
    * changes after code review
    
    * changes as per code review

---
+++ cron_scripts/inactive_idle_accounts.pl
@@ -0,0 +1,183 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use JSON;
+use File::Slurp qw(read_file);
+use POSIX qw(strftime);
+use Pod::Usage qw(pod2usage);
+use FindBin;
+
+=head1 NAME
+
+inactive_idle_accounts.pl - to make idle accounts to inactive status
+
+=head1 SYNOPSIS
+
+  inactive_idle_accounts.pl [-help] [-debug] -last_login_days <numDays> -new_status <zimbraAccountStatus>
+
+=head1 DESCRIPTION
+
+This script will search through Zimbra's Internal LDAP via SOAP API for accounts that have a last login older than the specified days.
+Matching accounts will have their account status updated to the value provided by the -new_status argument.
+
+=head1 OPTIONS
+
+=over
+
+=item -help
+
+Prints the help message.
+
+=item -last_login_days I<numDays>
+
+Specifies number of days to check for last login.
+
+=item -new_status I<zimbraAccountStatus>
+
+Specifies new status that should be changed to target users.
+
+=item -debug
+
+Enable debug output.
+
+=back
+
+=cut
+
+use lib "$FindBin::Bin/lib";
+use ZCS::CustomAPI;
+my $config_file = "$FindBin::Bin/config/config.json";
+
+my %opts = (
+    last_login_days => 97,
+    new_status      => 'locked'
+);
+
+GetOptions( \%opts, "help", "debug", "last_login_days=i", "new_status=s" )
+  or pod2usage( -verbose => 99, -sections => "SYNOPSIS", -exitval => 1 );
+
+pod2usage(1) if ( $opts{help} );
+
+my $conf_content = scalar read_file( $config_file, err_mode => 'carp' )
+  or die("Unable to read config file.\n");
+my $config = JSON->new->utf8->relaxed->decode($conf_content);
+unless ( $config->{MAILSTOREHOST} ) {
+    pod2usage(
+        -exitval => 1,
+        -msg     => "MAILSTOREHOST is not defined in config file."
+    );
+}
+unless ( $config->{MAILSTOREUSER} ) {
+    pod2usage(
+        -exitval => 1,
+        -msg     => "MAILSTOREUSER is not defined in config file."
+    );
+}
+unless ( $config->{MAILSTOREPWD} ) {
+    pod2usage(
+        -exitval => 1,
+        -msg     => "MAILSTOREPWD is not defined in config file."
+    );
+}
+
+my $new_status;
+my $last_login_days;
+if ( $opts{last_login_days} > 0 ) {
+    $last_login_days = $opts{last_login_days};
+}
+else {
+    pod2usage(
+        -exitval => 1,
+        -msg     => "last_login_days should be greater than 0."
+    );
+}
+my @allowed_status =
+  ( 'locked', 'active', 'pending', 'lockout', 'maintenance', 'closed' );
+if ( grep { $_ eq $opts{new_status} } @allowed_status ) {
+    $new_status = $opts{new_status};
+}
+else {
+    pod2usage(
+        -exitval => 1,
+        -msg =>
+"new_status '$opts{new_status}' is invalid, use one of the following: " . join(", ", @allowed_status)
+    );
+}
+
+my $last_login_timestamp =
+  strftime( '%Y%m%d000000',
+    localtime( time() - $last_login_days * ( 24 * 60 * 60 ) ) )
+  . "Z";
+
+my $soap_api = ZCS::CustomAPI->new(
+    conf => {
+        SOAPURI => "https://"
+          . $config->{MAILSTOREHOST}
+          . ":7071/service/admin/soap",
+        SOAPUser => $config->{MAILSTOREUSER},
+        SOAPPass => $config->{MAILSTOREPWD},
+    },
+    debug => ( $opts{debug} ) ? 1 : 0
+);
+
+# search for 'active' accounts whose last login is earlier than specified days $last_login_days
+my @data;
+if ($soap_api) {
+    my %args = (
+        "query" =>
+"(&(zimbraLastLogonTimestamp<=$last_login_timestamp)(zimbraAccountStatus=active)(!(zimbraIsAdminAccount=TRUE))(!(zimbraIsDelegatedAdminAccount=TRUE)))",
+        "attrs" => "uid,zimbraId,zimbraAccountStatus,zimbraLastLogonTimestamp"
+    );
+
+    if ( my $searched_users = eval { $soap_api->searchdirectory(%args) } ) {
+        my $path = "/Envelope/Body/SearchDirectoryResponse";
+        my $i    = 0;
+        foreach my $acc ( $searched_users->dataof("$path/account") ) {
+            $i++;
+            my $msom = $searched_users->match("$path/[$i]")
+              or die("MATCH '$path/[$i]' failed\n");
+
+            push @data, $acc->attr;
+        }
+    }
+    else {
+        pod2usage(
+            -exitval => 1,
+            -msg     => "SearchDirectoryRequest: "
+              . $soap_api->Error . " via "
+              . $config->{MAILSTOREHOST}
+        );
+    }
+
+    if ( scalar @data != 0 ) {
+        foreach my $acc (@data) {
+            my $zimbraId = $acc->{id};
+            my $modify_data = { 'zimbraAccountStatus' => $new_status };
+            if ( my $modify_acc =
+                eval { $soap_api->modifyaccount( $zimbraId, $modify_data ) } )
+            {
+                warn("DEBUG: $acc->{name} made inactive\n")
+                  if ( $soap_api->Debug );
+            }
+            else {
+                pod2usage( -msg => "ModifyAccountRequest: "
+                      . $soap_api->Error . " via "
+                      . $config->{MAILSTOREHOST}
+                      . "for user: "
+                      . $acc->{name} );
+            }
+        }
+    }
+    else {
+        warn("DEBUG: No users found! \n") if ( $soap_api->Debug );
+    }
+}
+else {
+    pod2usage(
+        -exitval => 1,
+        -msg     => ZCS::CustomAPI->Error
+    );
+}
---
+++ cron_scripts/lib/ZCS/CustomAPI.pm
@@ -0,0 +1,299 @@
+package ZCS::CustomAPI;
+
+use strict;
+use warnings;
+
+use SOAP::Lite ();
+use Data::Dumper;
+
+our $Debug = 0;
+our $Error = '';
+
+BEGIN {
+    # avoid self signed SSL certificate rejection
+    $ENV{"PERL_LWP_SSL_VERIFY_HOSTNAME"} = 0;
+
+    # avoid default SSL_verify_mode SSL_VERIFY_NONE deprecated warning
+    use LWP::Protocol::http ();
+    @LWP::Protocol::http::EXTRA_SOCK_OPTS = ( SSL_verify_mode => 0 );
+}
+
+=head1 NAME
+
+ZCS::CustomAPI - perl module for accessing Zimbra SOAP API
+
+This program extracted required features from https://github.com/plobbes/zcs-api/blob/master/lib/ZCS/API.pm
+
+=cut
+
+sub new {
+    my $class = shift;
+    die("new: invalid arguments\n") if ( @_ % 2 );
+
+    my $self = bless( {}, ref($class) || $class );
+    my @args = @_;
+    my ( $err, $key, $val );
+    for ( my $i = 0 ; $i < $#args ; $i += 2 ) {
+        ( $key, $val ) = ( $args[$i], $args[ $i + 1 ] );
+        local ($@);
+        eval {
+            my $func = "arg_$key";
+            $self->$func($val);
+        };
+        $err = $@;
+        last if $err;
+    }
+    if ($err) {
+        chomp($err);
+        die("new: $key($val) failed: $err\n");
+    }
+
+    # attempt to populate conf via ENV if necessary
+    $self->conf( {} ) unless ( $self->conf );
+
+    return $self;
+}
+
+sub arg_conf  { shift->conf(@_); }
+sub arg_debug { shift->Debug(@_); }
+
+sub Debug {
+    $Debug = $_[1] if ( @_ > 1 );
+    return $Debug;
+}
+
+sub Error {
+    $Error = $_[1] if ( @_ > 1 );
+    return $Error;
+}
+
+sub conf_keys { return qw(SOAPURI SOAPUser SOAPPass); }
+
+sub conf {
+    my ( $self, $conf ) = @_;
+    if ( @_ > 1 ) {
+        die("conf not a HASHREF\n") unless ( ref($conf) eq "HASH" );
+        my @req = $self->conf_keys;
+        my @err;
+        foreach my $k (@req) {
+            push( @err, $k ) unless ( defined( $conf->{$k} ) );
+        }
+        die( ref($self) . "->conf: missing info: ", join( ", ", @err ), "\n" )
+          if @err;
+        $self->{_conf} = $conf;
+    }
+    return $self->{_conf};
+}
+
+sub soap {
+    my $self = shift;
+    unless ( exists( $self->{_soap} ) ) {
+
+        #SOAP::Lite->import( +trace => "all" ) if $self->Debug;
+        $self->{_soap} = SOAP::Lite->new(
+            proxy    => $self->conf->{SOAPURI},
+            autotype => 0
+        );
+    }
+    return $self->{_soap};
+}
+
+sub soap_call {
+    my $self = shift;
+    my %opt  = @_;
+
+    my $req = SOAP::Data->name( $opt{req} );
+    $req->attr( $opt{attr} ) if $opt{attr};
+
+    my @body = ref $opt{body} eq "ARRAY" ? @{ $opt{body} } : ( $opt{body} );
+    my $resp;
+
+    my ( $err, $try, $maxtry, $sec ) = ( undef, 0, 3, 0 );
+    while ( !$resp && $try++ < $maxtry ) {
+        local ($@);
+        eval { $resp = $self->soap->call( $req, $opt{head}, @body ); };
+        $err = $@;
+        if ( _err($resp) =~ /Client auth credentials have expired/ ) {
+
+            # reauthenticate for long running processes
+            $opt{head} = $self->reauth( $opt{head} );
+            $resp = undef;    # force retry
+        }
+        elsif ($err) {
+            chomp($err);
+            last unless ( $try < $maxtry );
+            $sec += $try;    # back off a little more on each retry
+            warn("soap_call $opt{req}: sleep($sec): try#$try error: $err\n");
+            sleep($sec);
+        }
+    }
+
+    if ( $err || $resp->fault ) {
+        $self->Error( "error: $opt{req}"
+              . ( $try > 1 ? " (try #$try)" : "" ) . ": "
+              . ( $err || _err($resp) ) );
+        return undef;
+    }
+
+    return $resp;
+}
+
+sub _err {
+    my ($resp) = @_;
+    return ("no response object") unless ($resp);
+    if ( $resp->fault ) {
+        return (
+            (
+                     $resp->faultcode
+                  || $resp->valueof('/Envelope/Body/Fault/Detail/Error/Code')
+            )
+            . " "
+              . (
+                     $resp->faultstring
+                  || $resp->valueof('/Envelope/Body/Fault/Reason/Text')
+              )
+              . (
+                $Debug
+                ? ( "; "
+                      . $resp->fault->{detail}->{Error}->{Code} . ": "
+                      . $resp->fault->{detail}->{Error}->{Trace} )
+                : ""
+              )
+        );
+    }
+    if ( defined( $resp->valueof("//Fault/faultcode") ) ) {
+        return (
+                $resp->valueof("//Fault/faultcode") . " "
+              . $resp->valueof("//Fault/faultstring")
+              . (
+                $Debug
+                ? ( "; "
+                      . $resp->valueof('//Fault/detail/Error/Code') . ": "
+                      . $resp->valueof('//Fault/detail/Error/Trace') )
+                : ""
+              )
+        );
+    }
+}
+
+# expect no issues with token expiring
+sub auth {
+    my $self = shift;
+    unless ( exists( $self->{_zimbra_auth} ) ) {
+
+        # authenticate with zimbra to get AuthToken
+        my $req = "AuthRequest";
+        my $attr = { "xmlns" => "urn:zimbraAdmin" };
+        my $head =
+          SOAP::Header->name("context")->attr( { "xmlns" => "urn:zimbra" } );
+        my $body = [
+            SOAP::Data->name( name => $self->conf->{SOAPUser} )->type("string"),
+            SOAP::Data->name( password => $self->conf->{SOAPPass} )
+              ->type("string")
+        ];
+
+        my $resp = $self->soap_call(
+            req  => $req,
+            attr => $attr,
+            head => $head,
+            body => $body
+        );
+        die $self->Error unless $resp;
+
+        # Convert authToken into value that can be passed to zimbra requests
+        # and cache for all future requests
+        $self->{_zimbra_auth} = SOAP::Header->name(
+            "context" => \SOAP::Header->value(
+                SOAP::Header->name(
+                    "authToken" => $resp->valueof('//authToken')
+                ),
+            )
+        )->attr( { xmlns => "urn:zimbra" } );
+    }
+    return $self->{_zimbra_auth};
+}
+
+=head2 reauth(auth)
+
+Must pass the auth value that you want to reauthenticate.
+
+This shouldn't ever need to be called directly soap_call should detect when a
+token is no longer valid and automatically attempt to generate a new one
+using this method.
+
+  $za->reauth($self->auth);                         # re-auth admin
+
+=cut
+
+sub reauth {
+    my ( $self, $auth ) = @_;
+    if ( $auth == $self->{_zimbra_auth} ) {
+        delete( $self->{_zimbra_auth} );
+        return $self->auth;
+    }
+}
+
+sub getaccount {
+    my ( $self, $account ) = @_;
+
+    my $req = "GetAccountRequest";
+    my $attr = { "xmlns" => "urn:zimbraAdmin" };
+    my $body =
+      SOAP::Data->name( account => $account )->type("string")
+      ->attr( { by => "name" } );
+
+    return $self->soap_call(
+        req  => $req,
+        attr => $attr,
+        head => $self->auth,
+        body => $body
+    );
+}
+
+sub modifyaccount {
+    my ( $self, $id, $data ) = @_;
+
+    #my $resp = $self->getaccount($account);
+
+    my @attrs;
+    foreach my $a ( keys %$data ) {
+        push( @attrs,
+            SOAP::Data->name( a => $data->{$a} )->type("string")
+              ->attr( { n => $a } ) );
+    }
+
+    my $req  = "ModifyAccountRequest";
+    my $attr = {
+        "xmlns" => "urn:zimbraAdmin",
+        "id"    => $id
+    };
+
+    return $self->soap_call(
+        req  => $req,
+        attr => $attr,
+        head => $self->auth,
+        body => \@attrs
+    );
+}
+
+sub searchdirectory {
+    my ( $self, %args ) = @_;
+
+    my $req  = "SearchDirectoryRequest";
+    my $attr = {
+        "xmlns" => "urn:zimbraAdmin",
+        "types" => "accounts",
+        "query" => $args{query},
+        'attrs' => "$args{attrs}"
+    };
+    my $body = SOAP::Data->type( "xml" => "" );
+
+    return $self->soap_call(
+        req  => $req,
+        attr => $attr,
+        head => $self->auth,
+        body => $body
+    );
+}
+
+1;
